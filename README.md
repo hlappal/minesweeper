ff32f174dbc3da582b532da9d87a7fe3f48d05a8
Heikki Lappalainen <heikki.lappalainen@protonmail.com>
2022-04-22 21:30:37 +0300

    Update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # minesweeper
 ## A minesweeper game project.
 
-Just-for-fun project for recreating the classic Minesweeper game in Python 3.10.
+Just-for-fun project for recreating the classic Minesweeper game in Python 3.10, following the tutorial at [https://www.freecodecamp.org/news/object-oriented-programming-with-python-code-a-minesweeper-game/](https://www.freecodecamp.org/news/object-oriented-programming-with-python-code-a-minesweeper-game/).
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-from tkinter import Tk
+from tkinter import Tk, Frame
 
-root = Tk()
 
-root.mainloop()
+def main():
+    # Configure the window settings
+    root = Tk()
+    root.configure(bg="#afafaf")
+    root.geometry('980x640')
+    root.resizable(False, False)
+    root.title('Minesweeper')
+
+    top_frame = Frame(
+        root,
+        bg="#a0a0a0",
+        width=980,
+        height=30
+    )
+    top_frame.place(x=0, y=0)
+
+    # Run the window
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
